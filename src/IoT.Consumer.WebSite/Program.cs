c21e90c2559ac5d59e38d6d8e7a1cc5c5e333eb9
Andres Padilla <69865428+AndresPad@users.noreply.github.com>
2022-02-11 10:38:26 -0500

    temp-ui

---
+++ src/IoT.Consumer.WebSite/Program.cs
@@ -0,0 +1,36 @@
+using IoT.Consumer.WebSite.Data;
+using IoT.Consumer.WebSite.Events;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<IEventReaderService, EventReaderService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
