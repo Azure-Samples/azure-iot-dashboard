91106b5e4a5732d614fb0992c4958a9f6158737d
Raúl Alarcón <ralarcon@microsoft.com>
2022-02-11 12:50:17 +0100

    Initial startup sample code

---
+++ src/IoT.Consumer.WebSite/Events/IEventReaderService.cs
@@ -0,0 +1,8 @@
+﻿namespace IoT.Consumer.WebSite.Events
+{
+    public interface IEventReaderService : IAsyncDisposable
+    {
+        IObservable<Event> Events { get; }
+        IObservable<EventProcessorInfo> ProcessorInfo { get; }
+    }
+}
